b6ece43d2041cab84191fa34a6c8bdb8c5d61bda
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-11-25 19:22:35 +0100

    Added the enemies script, that includes a chasing script and a visible range.

---
+++ 4ElementsToDie/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	// Enemy base stats, they're fixed through the game so we can declare them as constants.
+	private const int baseVitality = 2;
+	private const int baseAttack = 2;
+	private const int baseDefense = 2;
+	private const int baseSpeed = 2;
+
+	// Enemy base hidden stats, they're fixed through the game.
+	private const int baseLuck = 2; 
+	private const int baseAttackSpeed = 2;
+	private const int baseAttackRange = 2;
+
+	// Enemy onGame visible stats.
+	public int mVitality {get; private set;}
+	public int mAttack {get; private set;}
+	public int mDefense {get; private set;}
+	public int mSpeed {get; private set;}
+
+	// Enemy onGame hidden stats.
+	public int mLuck { get; private set;}
+	public int mAttackSpeed { get; private set;}
+	public int mAttackRange { get; private set;}
+
+	// Unity objects and variables.
+	Transform tr;
+	float mHorizontalAttack = 0f;
+	float mVerticalAttack = 0f;
+	Animator mAnimator;
+	GameObject player;
+
+	// Facing and chasing variables.
+	bool mFacingRight;
+	bool mFacingUp;
+	bool chasePlayer;
+
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform> () as Transform;
+		mAnimator = GetComponent<Animator> () as Animator;
+		player  = GameObject.FindGameObjectWithTag ("Player");
+		FillWithBaseStats ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Fixed update because the Enemy can
+	void FixedUpdate() {
+		chasePlayer = false;
+
+		if (EnemyMovement.calculateDistance (tr, player.transform)) {
+			bool[] facings = EnemyMovement.Move (tr, mSpeed, mFacingRight, mFacingUp, player.transform);
+			mFacingRight = facings [0];
+			mFacingUp = facings [1];
+			chasePlayer = true;
+		} 
+			
+		EnemyAnimation.Animate (mAnimator, chasePlayer);
+			
+		// Attacking.
+	}
+
+	// Fills all stats with the base values.
+	void FillWithBaseStats(){
+		mVitality = baseVitality;
+		mAttack = baseAttack;
+		mDefense = baseDefense;
+		mSpeed = baseSpeed;
+		mLuck = baseLuck;
+		mAttackSpeed = baseAttackSpeed;
+		mAttackRange = baseAttackRange;
+
+		mFacingRight = true;
+		mFacingUp = false;
+	}
+}
---
+++ 4ElementsToDie/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement {
+
+	public static bool[] Move(Transform enemyTr, int enemySpeed, bool enemyFacingRight, bool enemyFacingUp, Transform playerTr) {
+
+		Vector3 chasingDirection = 
+			(playerTr.position - enemyTr.position).normalized;
+
+		// Moving the enemy.
+		enemyTr.position += chasingDirection * enemySpeed * Time.fixedDeltaTime;
+		
+		// Flip enemy horizontally
+		if ( (chasingDirection.x < 0) && (enemyFacingRight) ){
+			Vector3 ls = enemyTr.localScale;
+
+			ls.x = -1f * ls.x;
+			enemyTr.localScale = ls;
+			enemyFacingRight = !enemyFacingRight;
+
+
+		} else if ( (chasingDirection.x > 0) && (!enemyFacingRight) ) {
+			Vector3 ls = enemyTr.localScale;
+
+			ls.x = -1f * ls.x;
+			enemyTr.localScale = ls;
+			enemyFacingRight = !enemyFacingRight;
+
+		}
+
+		// Flip enemy vertically
+		if ( (chasingDirection.y < 0) && (enemyFacingUp) ){
+			enemyFacingUp = !enemyFacingUp;
+
+
+
+		} else if ( (chasingDirection.y > 0) && (!enemyFacingUp) ) {
+			enemyFacingUp = !enemyFacingUp;
+
+		}
+
+		return new bool[]{enemyFacingRight,enemyFacingUp};
+	}
+
+	// This functions calculates if the user is inside the enemy vision radius. 
+	// A circle is drawed with center enemy position and radio r.
+	// The point to check is player position.
+	public static bool calculateDistance(Transform enemyTr, Transform playerTr) {
+		float center_x = enemyTr.position.x; float center_y = enemyTr.position.y; 
+		float x = playerTr.position.x; float y = playerTr.position.y;
+		float radius = 5f;
+		return ( Mathf.Pow(x - center_x,2) + Mathf.Pow(y - center_y,2) ) <= Mathf.Pow(radius,2);
+	}
+}
