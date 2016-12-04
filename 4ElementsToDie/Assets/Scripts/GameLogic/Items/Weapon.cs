a6b9463131c9d8f0c42c7cf8371aae8ac92a3b3c
BelluschiMarco <marco1.belluschi@mail.polimi.it>
2016-12-05 00:46:19 +0100

    gameLogic Branch

---
+++ 4ElementsToDie/Assets/Scripts/GameLogic/Items/Weapon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu (menuName = "Item > Equipment > Weapon")]
+public class Weapon : Equipment {
+
+    [Header("AttackType")]
+    public AttackType attackType;
+
+}
