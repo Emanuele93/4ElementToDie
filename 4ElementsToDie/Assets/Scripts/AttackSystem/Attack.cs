a3bfce82d81312a9a304290fa08c41f8fb22ae91
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-12-07 12:53:29 +0100

    Refactored code, now the Attack abstract class includes common implementation that deriving classes use.

--- 4ElementsToDie/Assets/Scripts/AttackSystem/Attack.cs
+++ 4ElementsToDie/Assets/Scripts/AttackSystem/Attack.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attack : MonoBehaviour {
+[RequireComponent (typeof (PolygonCollider2D),typeof(Rigidbody2D))]
+public abstract class Attack : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	protected Rigidbody2D rb;
+	protected Transform tr;
+
+	protected float waitTime = 3f;
+
+	protected CharacterManager attacker;
+
+	protected virtual void Start() {
+		tr = GetComponent<Transform> () as Transform;
+		rb = GetComponent<Rigidbody2D> () as Rigidbody2D;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FixedUpdate() {
+		AttackNow ();
+	}
+
+	// the attack method is called when the user presses the attack button.
+	public virtual void AttackNow() {
+		StartCoroutine( ExplosionTime ());
+	}
+
+	protected IEnumerator ExplosionTime () {
+		yield return new WaitForSeconds (waitTime);
+		gameObject.SetActive (false);
+	}
+
+	// virtual in order to be overriden if necessary.
+	protected virtual void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Enemy") {
+			
+			Debug.LogError ("Enemy");
+
+			//CharacterManager defender = other.GetComponent<CharacterManager> () as CharacterManager;
+			//GameplayManager.Instance.SuccessfulAttack (attacker, defender);
+			//
+			//			
+			//
+			//			double damage = GameLogicManager.CalculateDamage (attacker, defender);
+			//			defender.ApplyDamage(damage);
+			//			if (GameLogicManager.IsDead(defender))
+			//			{
+			//				GameplayManager.Instance.Kill(defender);
+			//			}
+		} 
 	}
 }
