125f6c18759e82adc06750fca354a7124f5a5da1
Emanuele <emanuele.lattanzio@mail.polimi.it>
2016-12-07 10:00:18 +0100

    walls tag

--- 4ElementsToDie/Assets/Scripts/Map/door/bossDoor.cs
+++ 4ElementsToDie/Assets/Scripts/Map/door/bossDoor.cs
@@ -7,9 +7,9 @@ public class bossDoor : door
     {
         if (inDoorArea && Input.GetKeyDown(KeyCode.F))
         {
-            Vector3 mouvement = new Vector3(0, 4, 0);
+            Vector3 mouvement = new Vector3(0, 5, 0);
             player.transform.position = transform.rotation * mouvement + transform.position;
-            mouvement = new Vector3(0, 7, 0);
+            mouvement = new Vector3(0, 8, 0);
             mouvement = transform.rotation * mouvement + transform.position;
             Camera.main.transform.position = new Vector3(mouvement.x, mouvement.y, Camera.main.transform.position.z);
 
