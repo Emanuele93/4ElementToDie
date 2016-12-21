4cf8a126d7e21226e4b63c90cf8370006f167c2d
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2016-12-21 16:54:27 +0100

    Now the area attack collider fits the sprite and grows with it.

--- 4ElementsToDie/Assets/Scripts/AttackSystem/AreaAttack.cs
+++ 4ElementsToDie/Assets/Scripts/AttackSystem/AreaAttack.cs
@@ -4,27 +4,37 @@ using System.Collections;
 [RequireComponent (typeof(CircleCollider2D), typeof(SpriteRenderer))]
 public class AreaAttack : Attack {
 	CircleCollider2D col;
-	float colliderRadius = 3f;
+	float colliderRadius = 0f;
 
 	public Sprite attackSprite;
+
+	Vector3 initialScale = new Vector3 (0f,0f,0f);
+	Vector3 updateScale = new Vector3 (0f,0f,0f);
 
 	// Use this for initialization
 	protected override void Start ()
     {
 		base.Start ();
+		sr.sprite = attackSprite;
 
 		col = GetComponent<CircleCollider2D> () as CircleCollider2D;
-
-		col.radius = colliderRadius;
 		col.isTrigger = true;
+	}
 
+	void OnEnable() {
 		sr.sprite = attackSprite;
+		sr.transform.localScale = initialScale;
+		updateScale.x = 0f;
+		updateScale.y = 0f;
 	}
     
     public override void AttackNow()
     {
         base.AttackNow();
 
-        col.radius = attRange;
+		updateScale.x += Time.fixedDeltaTime / attRange;
+		updateScale.y += Time.fixedDeltaTime / attRange;
+		sr.transform.localScale += updateScale;
+
     }
 }
