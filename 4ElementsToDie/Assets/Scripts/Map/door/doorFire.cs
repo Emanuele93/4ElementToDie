44f3d30b00d5c708d3b17bc70cb5961ac1e1e9cd
Emanuele <emanuele.lattanzio@mail.polimi.it>
2017-01-08 13:27:21 +0100

    Some Fixes
    - No F on door if you can't open it
    - Added some sprites on shop boss area and initial room
    - Change backgrond color in different areas

--- 4ElementsToDie/Assets/Scripts/Map/door/doorFire.cs
+++ 4ElementsToDie/Assets/Scripts/Map/door/doorFire.cs
@@ -15,16 +15,17 @@ public class doorFire : MonoBehaviour
 
     void Update()
     {
-        if (inDoorArea && Input.GetKeyDown(KeyCode.F) && player.GetComponent<CharacterManager>().Stones[(int)ElementType.Fire] > 0)
+        if (inDoorArea && Input.GetKeyDown(KeyCode.F))
         {
             Vector3 mouvement = new Vector3(0, 3.5f, 0);
             player.transform.position = transform.rotation * mouvement + transform.position;
             mouvement = new Vector3(0, 6, 0);
             mouvement = transform.rotation * mouvement + transform.position;
             Camera.main.transform.position = new Vector3(mouvement.x, mouvement.y, Camera.main.transform.position.z);
+            Camera.main.backgroundColor = new Color(0.475f, 0.165f, 0.173f);
 
-			// Changing the sound.
-			GameplayManager.Instance.PlayMusic(Constants.MUSIC_FireArea);
+            // Changing the sound.
+            GameplayManager.Instance.PlayMusic(Constants.MUSIC_FireArea);
 
             transform.parent.gameObject.SetActive(false);
             where.SetActive(true);
@@ -33,7 +34,7 @@ public class doorFire : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && other.gameObject.GetComponent<CharacterManager>().Stones[(int)ElementType.Fire] > 0)
         {
             buttom.SetActive(true);
             player = other.gameObject;
--- 4ElementsToDie/Assets/Scripts/Object/stones/stoneAir.cs
+++ 4ElementsToDie/Assets/Scripts/Object/stones/stoneAir.cs
@@ -21,6 +21,7 @@ public class stoneAir : MonoBehaviour {
             other.gameObject.GetComponent<CharacterManager>().Stones[(int)ElementType.Air]++;
             other.transform.position = new Vector3(0, 0, 0);
             Camera.main.transform.position = new Vector3(0, 0, Camera.main.transform.position.z);
+            Camera.main.backgroundColor = new Color(0, 0, 0);
             transform.parent.parent.GetComponent<superMap>().centralRoomObject.SetActive(true);
             transform.parent.parent.gameObject.SetActive(false);
             Destroy(gameObject);
