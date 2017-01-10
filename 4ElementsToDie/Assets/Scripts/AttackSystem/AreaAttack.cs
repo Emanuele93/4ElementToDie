d919f0c59ce4a441f49506ff5f8b8bb22e75595e
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-01-10 01:47:30 +0100

    Changed the sprite and behaviour of area attacks, now the collider fits the sprite and we resize the image, thus, resizing the collider with it.

--- 4ElementsToDie/Assets/Scripts/AttackSystem/AreaAttack.cs
+++ 4ElementsToDie/Assets/Scripts/AttackSystem/AreaAttack.cs
@@ -3,42 +3,38 @@ using System.Collections;
 
 [RequireComponent (typeof(CircleCollider2D), typeof(SpriteRenderer))]
 public class AreaAttack : Attack {
-
 	CircleCollider2D col;
-
-	float baseRadius = 2.5f;
 
 	public Sprite attackSprite;
 
-	Vector3 initialScale = new Vector3 (0f,0f,0f);
+	Vector3 initialScale = new Vector3 (1f,1f,1f);
 	Vector3 updateScale = new Vector3 (0f,0f,0f);
 
 	// Use this for initialization
 	protected override void Start ()
-    {
+	{
 		base.Start ();
-		sr.sprite = attackSprite;
 
 		col = GetComponent<CircleCollider2D> () as CircleCollider2D;
-
-		col.radius = baseRadius;
 		col.isTrigger = true;
 	}
 
-	void OnEnable() {
-		sr.sprite = attackSprite;
+	protected override IEnumerator Fade() {
+
+		yield return new WaitForSeconds(waitTime);
 		sr.transform.localScale = initialScale;
 		updateScale.x = 0f;
 		updateScale.y = 0f;
+		gameObject.SetActive(false);
 	}
-    
-    public override void AttackNow()
-    {
-        base.AttackNow();
-		updateScale.x += (Time.fixedDeltaTime * attRange) / 50;
-		updateScale.y += (Time.fixedDeltaTime * attRange) / 50;
-		sr.transform.localScale += updateScale;
 
-        col.radius = baseRadius * attRange;
-    }
+	public override void AttackNow()
+	{
+		attRange = (float)GameplayManager.Instance.attackersDict[gameObject.GetInstanceID()].Stats[(int)StatType.AttRNG].FinalStat;
+		updateScale.x += (Time.fixedDeltaTime * attRange) / 5;
+		updateScale.y += (Time.fixedDeltaTime * attRange) / 5;
+		sr.transform.localScale += updateScale;
+		StartCoroutine (Fade ());
+
+	}
 }
